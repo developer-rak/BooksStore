165d7dff029c18c2d8cb36f0e495cf2501a70e25
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-06-20 16:07:45 +0500

    all files

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ $(window).scroll(function () {
     let oTop = $(".section-2").offset().top - window.innerHeight;
     if ($(window).scrollTop() > oTop) {
         navbar.addClass("sticky");
-    }
-    else {
+    } else {
         navbar.removeClass("sticky");
     }
 });
